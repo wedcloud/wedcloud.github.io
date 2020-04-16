5465cf5c6c6cd86da2e8544cadc17a376ea8624f
wedcloud <xuhb@hitrobotgroup.com>
2020-04-17 00:05:42 +0800

    spring-aop

---
+++ spring-aop/src/main/java/com/wedcloud/dynamicproxy/Host.java
@@ -0,0 +1,17 @@
+package com.wedcloud.dynamicproxy;
+
+/**
+ * @Author 许海斌
+ * @create 2020/4/16 0016 23:13
+ */
+public class Host implements Rent{
+    private static final int m=1000;
+    @Override
+    public void rent(int money) {
+        if(money>m) {
+            System.out.println("房东出租房子");
+        }else{
+            System.out.println("房东不出租房子");
+        }
+    }
+}
---
+++ spring-aop/src/main/java/com/wedcloud/dynamicproxy/Rent.java
@@ -0,0 +1,13 @@
+package com.wedcloud.dynamicproxy;
+
+/**
+ * @Author 许海斌
+ * @create 2020/4/16 0016 23:12
+ */
+public interface Rent {
+    /**
+     * 租房
+     * @param money
+     */
+    void rent(int money);
+}
