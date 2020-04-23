41489ed8e5ceaf721fa480b28dd5972f7140faa5
wedcloud <xuhb@hitrobotgroup.com>
2020-04-23 17:31:19 +0800

    springboot

---
+++ blog/source/_posts/springboot.md
@@ -0,0 +1,11 @@
+---
+title: springboot
+tags: [springboot]
+categories:
+  - springboot
+date: 2020-04-23 14:35:56
+---
+
+# springboot
+
+## 自动配置
---
+++ springboot/src/main/java/com/wedcloud/springboot/SpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.wedcloud.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot 启动主类
+ *
+ * @author spring
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootApplication.class, args);
+  }
+}
